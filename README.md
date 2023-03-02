ddd0471ff50b8a9f05b158aac015b2da477efb9a
Hikaru Terazono (3c1u) <3c1u@tohkani.com>
2023-03-02 17:39:36 +0900

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## junit-parser
+
+Node.js module for parsing JUnit XML files.
+
+```
+pnpm install @techtrain/junit-parser
+```
---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+/// <reference types="vitest" />
+
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+import dts from 'vite-plugin-dts'
+
+export default defineConfig({
+  plugins: [dts()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/main.ts'),
+      name: '@techtrain/junit-parser',
+      fileName: 'junit-parser',
+      formats: ['es', 'cjs'],
+    },
+    rollupOptions: {
+      external: [],
+      output: {},
+    },
+  },
+  resolve: {
+    alias: {
+      '~': `${__dirname}/src`,
+    },
+  },
+  test: {
+    environment: 'node',
+    setupFiles: [],
+  },
+})
